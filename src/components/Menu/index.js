67cb1815ee0ff41aeeec0240720e028828a7059d
ThomasKubant <77358241+ThomasKubant@users.noreply.github.com>
2021-06-21 09:43:03 -0700

    trying to deploy

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -6,15 +6,15 @@ function closeMenu() {
 } 
 
 function Menu () {
-    // const [currentPage, setCurrentPage] = useState('home')
+    const [currentPage, setCurrentPage] = useState('home')
     return(
         <div className='overlay' id='menu'>
             <Button variant='danger' id='closeMenuBtn' onClick={closeMenu}>Close</Button>
             <div className='overlay-content'>
-                <h1>Home</h1>
-                <h1>About Me</h1>
-                <h1>Projects</h1>
-                <h1>Social Media</h1>
+                <h1 onClick={setCurrentPage('home')}>Home</h1>
+                <h1 onClick={setCurrentPage('about')}>About Me</h1>
+                <h1 onClick={setCurrentPage('projects')}>Projects</h1>
+                <h1 onClick={setCurrentPage('social')}>Social Media</h1>
             </div>
         </div>
     )
