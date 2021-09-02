75bbd161dc017bb038b63437cf27953963a2c018
ThomasKubant <77358241+ThomasKubant@users.noreply.github.com>
2021-09-01 20:01:43 -0700

    edited contact page a bit

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -17,25 +17,29 @@ function Contact() {
     e.target.reset();
   }
   return (
-    <div className="content">
-      <h1>Contact Me</h1>
-      <form id="contact-form" onSubmit={handleSubmit}>
-        <div className='contact-field'>
-          <label htmlFor="email">Email:</label>
-          <input type="email" name="email" />
-        </div>
-        <div className='contact-field'>
-          <label htmlFor="subject">Subject:</label>
-          <input type="text" name="subject" />
-        </div>
-        <div className='contact-field'>
-          <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" />
-        </div>
-        <button type="submit">
-          Submit
-        </button>
-      </form>
+    <div className="content">   
+      <div className="contact-page">
+        <h1>Contact Me</h1>
+        <h2>ThomasKubant@Gmail.com</h2>
+        <h2 className="or-h2"><span>OR</span></h2>
+        <form id="contact-form" onSubmit={handleSubmit}>
+          <div className='contact-field'>
+            <label htmlFor="email">Email:</label>
+            <input type="email" name="email" />
+          </div>
+          <div className='contact-field'>
+            <label htmlFor="subject">Subject:</label>
+            <input type="text" name="subject" />
+          </div>
+          <div className='contact-field'>
+            <label htmlFor="message">Message:</label>
+            <textarea name="message" rows="5" />
+          </div>
+          <button type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
       <div className="socialsBar">
         <Social></Social>
       </div>
